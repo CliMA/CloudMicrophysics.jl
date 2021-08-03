dbce5602ec84a86984f04abc77a319d2ae28eca3
Anna Jaruga <ajaruga@caltech.edu>
2021-08-02 19:20:34 -0700

    Add documentation

--- docs/make.jl
+++ docs/make.jl
@@ -1,9 +1,15 @@
+# https://github.com/jheinen/GR.jl/issues/278#issuecomment-587090846
+ENV["GKSwstype"] = "nul"
+
 using CloudMicrophysics, Documenter
 using DocumenterCitations
 
 bib = CitationBibliography(joinpath(@__DIR__, "bibliography.bib"))
 
-pages = Any["Home" => "index.md", "References" => "References.md"]
+pages = Any["Home" => "index.md",
+            "0-moment microphysics" => "Microphysics_0M.md",
+            "1-moment microphysics" => "Microphysics_1M.md",
+            "References" => "References.md"]
 
 mathengine = MathJax(Dict(
     :TeX => Dict(
@@ -21,7 +27,7 @@ format = Documenter.HTML(
 makedocs(
     bib,
     sitename = "CloudMicrophysics.jl",
-    strict = false, # TODO: make strict = true
+    strict = true,
     format = format,
     checkdocs = :exports,
     clean = true,
---
+++ docs/src/API.md
@@ -0,0 +1,41 @@
+```@meta
+CurrentModule = CloudMicrophysics
+```
+
+# 0-moment microphysics
+
+```@docs
+Microphysics_0M
+```
+
+## Methods
+
+```@docs
+Microphysics_0M.remove_precipitation
+```
+
+# 1-moment microphysics
+
+```@docs
+Microphysics_1M
+```
+
+## Methods
+
+```@docs
+Microphysics_1M.v0_rai
+Microphysics_1M.n0_sno
+Microphysics_1M.τ_relax
+Microphysics_1M.lambda
+Microphysics_1M.unpack_params
+Microphysics_1M.G_func
+Microphysics_1M.terminal_velocity
+Microphysics_1M.conv_q_vap_to_q_liq_ice
+Microphysics_1M.conv_q_liq_to_q_rai
+Microphysics_1M.conv_q_ice_to_q_sno
+Microphysics_1M.accretion
+Microphysics_1M.accretion_rain_sink
+Microphysics_1M.accretion_snow_rain
+Microphysics_1M.evaporation_sublimation
+Microphysics_1M.snow_melt
+```
