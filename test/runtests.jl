fe59784dce6ff0698b49ece9bbf8fdaa571d9ddd
odunbar <odunbar@caltech.edu>
2022-02-10 12:23:06 -0800

    now uses CLIMAParameters toml files

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,16 +1,9 @@
 #get parameters from file
-using TOML
-parameter_parse = TOML.parsefile(joinpath(@__DIR__, "parameters.toml"))
-src_parameter_dict = Dict{String, Float64}()
-for (key, val) in parameter_parse
-    # In the future - we will use the full names,
-    # src_parameter_dict[key] = val["value"]
+# read parameters needed for tests
+import CLIMAParameters
+src_parameter_dict = CLIMAParameters.create_parameter_dict(dict_type = "alias")
 
-    # for now we use the aliases
-    src_parameter_dict[val["alias"]] = val["value"]
-end
-
-include("parameter_tests.jl")
+#include("parameter_tests.jl")
 include("aerosol_activation_tests.jl")
 include("microphysics_tests.jl")
 include("gpu_tests.jl")
