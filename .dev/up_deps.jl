47e2bc11eb35d687b2129f51f0d8f71c752ff394
Anna Jaruga <ajaruga@caltech.edu>
2022-07-25 09:09:46 -0700

    add up_deps script

---
+++ .dev/up_deps.jl
@@ -0,0 +1,28 @@
+#=
+A simple script for updating the manifest
+files in all of our environments.
+=#
+
+root = dirname(@__DIR__)
+dirs = (
+    root,
+    joinpath(root, "test"),
+    joinpath(root, "perf"),
+    joinpath(root, "docs"),
+    joinpath(root, "integration_tests"),
+)
+
+cd(root) do
+    for dir in dirs
+        @info "Pkg.up for environment $dir"
+        cmd = `$(Base.julia_cmd()) --project=$dir -e 'import Pkg; Pkg.update()'`
+        run(cmd)
+    end
+end
+
+# https://github.com/JuliaLang/Pkg.jl/issues/3014
+for dir in dirs
+    cd(dir) do
+        rm("LocalPreferences.toml"; force = true)
+    end
+end
