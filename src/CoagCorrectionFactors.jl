421cc6e86dd579044e8330a171fee3880afc2879
nefrathenrici <nat.henrici@gmail.com>
2022-11-11 17:03:58 -0800

    Finish intramodal coag, add fn stub for intermodal

--- src/CoagCorrectionFactors.jl
+++ src/CoagCorrectionFactors.jl
@@ -10,7 +10,7 @@ Correction factors for 0-th and 6-th moment are the same in Whitby 91 - see p. H
 These correction factors are obtained from the CAM5 code for higher precision.
 """
 function intramodal_correction(stdev)
-      index = max(1, min(10, round((stdev - 0.75)/0.25)))
+      index = max(1, min(10, round(Integer, (stdev - 0.75)/0.25)))
       correction_factors = [
       0.707106785165097, 0.726148960080488, 0.766430744110958,
       0.814106389441342, 0.861679526483207, 0.903600509090092,   
--- src/Coagulation.jl
+++ src/Coagulation.jl
@@ -1,7 +1,24 @@
 module Coagulation
-
+include("Parameters.jl")
+include("Common.jl")
+include("CommonTypes.jl")
+include("AerosolModel.jl")
 include("CoagCorrectionFactors.jl")
 import .CoagCorrectionFactors as CCF
+
+import .AerosolModel as AM
+
+function whitby_coagulation(ad)
+    # TODO: add these values to climaparameters - this comes from CAM5
+    surface_temp = 288.15
+    surface_pressure = 101325.0
+    mean_free_path = 6.6328e-8 * surface_pressure * temp / (surface_temp * pressure)
+
+    # Call coags for aitken and accumulation modes:
+    
+end
+
+
 """
     whitby_intramodal_coag(ad, temp, rho_p)
 
@@ -19,18 +36,23 @@ function whitby_intramodal_coag(
     am,
     K_fm,
     K_nc,
-    ESG, 
+    ESG,
+    mean_free_path
 )
-    SQD_gn = sqrt(2 * am.r_dry)  
+    # Knudsen number (m)
+    Kn_g = mean_free_path /  am.r_dry
+    # Near-continuum factor
+    A_nc = 1.43 * Kn_g^0.0814
+    sqrt_diameter = sqrt(2 * am.r_dry)  
 
-    # Whitby H.11a
-    m_0_fm(am) = -am.N^2 * K_fm * CCF.intramodal_correction(am.stdev) * SQD_gn * (ESG + ESG^25 + 2 * ESG^5)
+    # Whitby H.11a 
+    m_0_fm(am) = -am.N^2 * K_fm * CCF.intramodal_correction(am.stdev) * sqrt_diameter * (ESG + ESG^25 + 2 * ESG^5)
     # Whitby H.11b
-    m_6_fm(am) = -2 * am.N^2 * K_fm * intramodal_correction(am.stdev) * SQD_gn^6 * (ESG^85 + 2*ESG^89 + ESG^109)
+    m_6_fm(am) = -2 * am.N^2 * K_fm * CCF.intramodal_correction(am.stdev) * sqrt_diameter^6 * (ESG^85 + 2*ESG^89 + ESG^109)
     # Whitby H.12a
-    m_0_nc(am) = -am.N^2 * K_nc * (1 + ESG^8 + A * Kn_g * (ESG^20 + ESG^4))
+    m_0_nc(am) = -am.N^2 * K_nc * (1 + ESG^8 + A_nc * Kn_g * (ESG^20 + ESG^4))
     #Whitby H.12b
-    m_6_nc(am) = -2 * am.N^2 * K_nc * D_gn^6 * (ESG^72 + ESG^80 + A * Kn_g * (ESG^52 + ESG^68))
+    m_6_nc(am) = -2 * am.N^2 * K_nc * (2*am.r_dry)^6 * (ESG^72 + ESG^80 + A_nc * Kn_g * (ESG^52 + ESG^68))
 
     return (
         m_0_fm(am),
@@ -38,6 +60,27 @@ function whitby_intramodal_coag(
         m_0_nc(am),
         m_6_nc(am)
     )
+end
+
+
+function whitby_intermodal_aitken_coag(
+    am,
+    K_fm,
+    K_nc,
+    ESG,
+    mean_free_path
+    )
+
+end
+
+function whitby_intermodal_accumulation_coag(
+    am,
+    K_fm,
+    K_nc,
+    ESG,
+    mean_free_path
+    )
+
 end
 
 end
