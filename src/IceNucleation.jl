731da1632e4b1b40f940a9c5fb21123848403c5f
amylu00 <alu3@caltech.edu>
2023-01-18 11:24:18 -0800

    Adding testing ice nucleation file

---
+++ src/IceNucleation.jl
@@ -0,0 +1,14 @@
+
+"""
+Parameterization for heterogenous ice nucleation. 
+"""
+
+"""
+Working on diff_activated_fraction function 
+"""
+function diff_activated_fraction(T::FT)
+    return 0
+end
+
+
+
