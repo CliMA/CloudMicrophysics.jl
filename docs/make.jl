e865af89371aa1a8f335e58f8bedc7b2870fb2db
amylu00 <alu3@caltech.edu>
2023-01-18 11:24:18 -0800

    Add activated fraction parameterization and a box model for testing

--- docs/make.jl
+++ docs/make.jl
@@ -15,6 +15,8 @@ pages = Any[
     "Smooth transition at thresholds" => "ThresholdsTransition.md",
     "Aerosol activation" => "AerosolActivation.md",
     "ARG2000 activation example" => "ARG2000_example.md",
+    "Ice Nucleation" => "IceNucleation.md",
+    "Box model ice nucleation example" => "IceNucleationBox.md",
     "API" => "API.md",
     "References" => "References.md",
 ]
