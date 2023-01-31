4011f68a4dcef416d9175db1848c112721635544
amylu00 <alu3@caltech.edu>
2023-01-30 23:39:46 -0800

    fixed citation, added mohler function

--- src/IceNucleation.jl
+++ src/IceNucleation.jl
@@ -1,13 +1,22 @@
-
 """
 Parameterization for heterogenous ice nucleation. 
 """
 
 """
-Working on diff_activated_fraction function 
+Working on mohler_activated_fraction function. Need to define a, S0, and Smax
 """
-function diff_activated_fraction(T::FT)
-    return 0
+function mohler_activated_fraction(
+    S0::FT, # threshold ice saturatio ratio
+    Smax::FT, # max ice saturation ratio. maybe make a separate function for this?
+    ice_sat_ratio::NTuple{N,T}
+) where {FT <: Real}
+
+    # at S >= S_max, there is a constant f_max
+    for (S) in ice_sat_ratio
+        AF = exp(a*(S - S0)) - 1;
+    end
+
+    return AF
 end
 
 
