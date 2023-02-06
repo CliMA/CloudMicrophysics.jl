a04f857b10b928339d1cfc4cd21252d5e3cd04b7
amylu00 <alu3@caltech.edu>
2023-02-06 08:28:41 -0800

    added a an S0 values

--- src/IceNucleation.jl
+++ src/IceNucleation.jl
@@ -3,21 +3,38 @@ Parameterization for heterogenous ice nucleation.
 """
 
 """
-Working on mohler_activated_fraction function. Need to define a, S0, and Smax
+Working on deposition_activated_fraction function. Check Smax and have AF be constant after reaching it?
 """
-function mohler_activated_fraction(
-    S0::FT, # threshold ice saturatio ratio
-    Smax::FT, # max ice saturation ratio. maybe make a separate function for this?
-    ice_sat_ratio::NTuple{N,T}
+function deposition_activated_fraction(
+    ice_sat_ratio::NTuple{N, T},
+    T::FT,              # Temp in Kelvins
+    dustType::Int32     # i.e. 1 would correspond to Arizona Test Dust, 2-->desert dust
 ) where {FT <: Real}
-
-    # at S >= S_max, there is a constant f_max
+    if dustType == 1    # Arizona Test Dust
+        S0 = 1.05;      # threshold ice saturatio ratio
+        if T > 220
+            a = 4.7;    # average at 223K
+        else
+            a = 9.2;    # average at 210K
+        end
+    end
+    if dustType == 2    # desert dust
+        if T > 220
+            a = 0.5;    # "less than 1"
+            S0 = 1.14;  # threshold ice saturatio ratio
+        else
+            a = 2;
+            S0 = 1.05;
+        end
+    end
     for (S) in ice_sat_ratio
-        AF = exp(a*(S - S0)) - 1;
+        AF = exp(a * (S - S0)) - 1;
     end
 
     return AF
 end
 
+function immersion_activated_fraction
 
+end
 
