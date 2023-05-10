24fe359cd3b98dc94d0a5a5ecb8e4b0b870b9f9c
Anna Jaruga <ajaruga@caltech.edu>
2023-05-09 22:45:41 -0700

    simplify noneq

--- src/MicrophysicsNonEq.jl
+++ src/MicrophysicsNonEq.jl
@@ -6,49 +6,34 @@
 """
 module MicrophysicsNonEq
 
-import Thermodynamics
-const TD = Thermodynamics
+import Thermodynamics as TD
 
-import ..CommonTypes
-const CT = CommonTypes
-
-import ..Parameters
-const CMP = Parameters
-const APS = CMP.AbstractCloudMicrophysicsParameters
+import ..CommonTypes as CT
+import ..Parameters as CMP
 
 export τ_relax
 export conv_q_vap_to_q_liq_ice
 
 """
-    τ_relax(param_set, liquid)
-    τ_relax(param_set, ice)
+    τ_relax(prs, liquid)
+    τ_relax(prs, ice)
 
- - `param_set` - abstract set with Earth parameters
- - `liquid` - a type for cloud liquid water
- - `ice` - a type for cloud ice
+ - `prs` - abstract set with Earth parameters
+ - `liquid` or `ice` - a type for cloud liquid water or cloud ice
 
 Returns the relaxation timescale for condensation and evaporation of
 cloud liquid water or the relaxation timescale for sublimation and
 deposition of cloud ice.
 """
-function τ_relax(param_set::APS, liquid::CT.LiquidType)
-
-    _τ_relax = CMP.τ_cond_evap(param_set)
-    return _τ_relax
-end
-function τ_relax(param_set::APS, ice::CT.IceType)
-
-    _τ_relax = CMP.τ_sub_dep(param_set)
-    return _τ_relax
-end
+τ_relax(prs, ::CT.LiquidType) = CMP.τ_cond_evap(param_set)
+τ_relax(prs, ::CT.IceType) = CMP.τ_sub_dep(param_set)
 
 """
     conv_q_vap_to_q_liq_ice(param_set, liquid, q_sat, q)
     conv_q_vap_to_q_liq_ice(param_set, ice, q_sat, q)
 
- - `param_set` - abstract set with Earth parameters
- - `liquid` - a type for cloud water
- - `ice` - a type for cloud ice
+ - `prs` - abstract set with Earth parameters
+ - `liquid` or `ice` - a type for cloud water or cloud ice
  - `q_sat` - PhasePartition at equilibrium
  - `q` - current PhasePartition
 
@@ -57,27 +42,9 @@ or cloud ice tendency due to sublimation and vapor deposition.
 The tendency is obtained assuming a relaxation to equilibrium with
 a constant timescale.
 """
-function conv_q_vap_to_q_liq_ice(
-    param_set::APS,
-    liquid::CT.LiquidType,
-    q_sat::TD.PhasePartition{FT},
-    q::TD.PhasePartition{FT},
-) where {FT <: Real}
-
-    _τ_cond_evap::FT = τ_relax(param_set, liquid)
-
-    return (q_sat.liq - q.liq) / _τ_cond_evap
-end
-function conv_q_vap_to_q_liq_ice(
-    param_set::APS,
-    ice::CT.IceType,
-    q_sat::TD.PhasePartition{FT},
-    q::TD.PhasePartition{FT},
-) where {FT <: Real}
-
-    _τ_sub_dep::FT = τ_relax(param_set, ice)
-
-    return (q_sat.ice - q.ice) / _τ_sub_dep
-end
+conv_q_vap_to_q_liq_ice(prs, liquid::CT.LiquidType, q_sat, q) =
+    (q_sat.liq - q.liq) / τ_relax(prs, liquid)
+conv_q_vap_to_q_liq_ice(prs, ice::CT.IceType, q_sat, q) =
+    (q_sat.ice - q.ice) / τ_relax(prs, ice)
 
 end #module MicrophysicsNonEq.jl
