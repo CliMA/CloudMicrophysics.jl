492a965e95fe01cfe47307cb2fc633e608dfbcab
Anna Jaruga <ajaruga@caltech.edu>
2023-05-12 17:11:22 -0700

    ...

--- src/MicrophysicsNonEq.jl
+++ src/MicrophysicsNonEq.jl
@@ -25,12 +25,12 @@ Returns the relaxation timescale for condensation and evaporation of
 cloud liquid water or the relaxation timescale for sublimation and
 deposition of cloud ice.
 """
-τ_relax(prs, ::CT.LiquidType) = CMP.τ_cond_evap(param_set)
-τ_relax(prs, ::CT.IceType) = CMP.τ_sub_dep(param_set)
+τ_relax(prs, ::CT.LiquidType) = CMP.τ_cond_evap(prs)
+τ_relax(prs, ::CT.IceType) = CMP.τ_sub_dep(prs)
 
 """
-    conv_q_vap_to_q_liq_ice(param_set, liquid, q_sat, q)
-    conv_q_vap_to_q_liq_ice(param_set, ice, q_sat, q)
+    conv_q_vap_to_q_liq_ice(prs, liquid, q_sat, q)
+    conv_q_vap_to_q_liq_ice(prs, ice, q_sat, q)
 
  - `prs` - abstract set with Earth parameters
  - `liquid` or `ice` - a type for cloud water or cloud ice
