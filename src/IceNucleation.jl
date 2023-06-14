31892030c4421d86c31a41f6e23124c277b9b040
amylu00 <alu3@caltech.edu>
2023-06-14 13:19:18 -0700

    added ABIFM_J to IceNucleation.jl

--- src/IceNucleation.jl
+++ src/IceNucleation.jl
@@ -5,6 +5,7 @@ module HetIceNucleation
 
 import ..CommonTypes as CT
 import ..Parameters as CMP
+import Thermodynamics as TD
 
 const APS = CMP.AbstractCloudMicrophysicsParameters
 
@@ -52,6 +53,38 @@ function dust_activated_number_fraction(
         a::FT = T > T_thr ? a_warm(prs, dust_type) : a_cold(prs, dust_type)
         return max(0, exp(a * (Si - S0)) - 1)
     end
+end
+
+"""
+   ABIFM_J(prs, x, T)
+
+ - `prs` - set with model parameters
+ - `x` - wt% sulphuric acid / 100
+ - `T` - air temperature [K]
+
+Returns the immersion freezing nucleation rate coefficient, `J`, in cm^-2 s^-1.
+`m` and `c` constants for illite are taken from Knopf & Alpert 2013 Table 1.
+"""
+function ABIFM_J(
+    prs::APS,
+    x::FT,
+    T::FT,
+) where {FT <: Real}
+    
+    m = 54.48075    # for illite particles
+    c = -10.66873
+
+    w_h = 1.4408 * x
+    p_sol = exp( 23.306 - 5.3465*x + 12*x*w_h - 8.19*x*w_h^2 + ( -5814 + 928.9*x - 1876.7*x*w_h )/T ) * 100 # * 100 converts mbar --> Pa
+    p_sat = TD.saturation_vapor_pressure(thermo_params, T, TD.Liquid())
+    p_ice = exp( 9.550426 - 5723.265/T + 3.53068*log(T) - 0.00728332*T ) # check with TD.saturation_vapor_pressure(thermo_params, T, TD.Ice())
+
+    a_w = p_sol / p_sat
+    a_w_ice = p_ice / p_sat
+    Delta_a_w = a_w - a_w_ice
+
+    logJ = m * Delta_a_w + c
+    return 10^logJ
 end
 
 end # end module
