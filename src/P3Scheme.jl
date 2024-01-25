eab4871a11dbc852c9f018917f9433b234c9a439
Anastasia Popova <apopova@caltech.edu>
2024-01-25 13:45:30 -0800

    solver gives some  numbers

--- src/P3Scheme.jl
+++ src/P3Scheme.jl
@@ -14,6 +14,7 @@ import Integrals as IN
 import RootSolvers as RS
 import CLIMAParameters as CP
 import CloudMicrophysics.Parameters as CMP
+import SpecialFunctions as SF
 
 
 const PSP3 = CMP.ParametersP3
@@ -200,6 +201,78 @@ function p3_mass(
     end
 end
 
+"""
+    μ_calc(λ)
+
+ - λ - slope parameter for gamma distribution of N′
+
+ Returns the shape parameter (μ) corresponding to the given λ value
+ Eq. 3 in Morrison and Milbrandt (2015).
+"""
+function μ_calc(λ::FT) where {FT}
+    μ = 0.00191 * λ^(0.8) - 2
+    if μ < 0
+        μ = FT(0)
+    elseif μ > 6
+        μ = FT(6)
+    end
+    return μ
+end
+
+"""
+    q_(p3, ρ, F_r, N_0, λ, μ, D_min, D_max)
+
+ - p3 - a struct with P3 scheme parameters
+ - ρ - bulk ice density (ρ_i for small ice, ρ_g for graupel) [kg/m^3]
+ - F_r - rime mass fraction [q_rim/q_i]
+ - N_0 - intercept parameter of N′ gamma distribution
+ - λ - slope parameter of N′ gamma distribution 
+ - μ - shape parameter of N′ gamma distribution
+ - D_min - minimum bound for regime 
+ - D_max - maximum bound for regime (if not specified, then infinity)
+
+ Returns the prognostic mass mixing ratio for a given regime
+"""
+# small, spherical ice or graupel (completely rimed, spherical)
+function q_s(ρ::FT, N_0::FT, λ::FT, μ::FT, D_min::FT, D_max::FT) where {FT} 
+    return (π/6 * ρ * N_0) * λ^(-1 * (μ + 4)) * (SF.gamma(μ + 4, λ*D_min) - SF.gamma(μ + 4, λ*D_max)) 
+end
+# dense, non-spherical ice
+function q_n(p3::PSP3, N_0::FT, λ::FT, μ::FT, D_min::FT, D_max::FT) where {FT} 
+    return (p3.α_va * N_0) * (λ)^(-1 * (μ + p3.β_va + 1)) * (SF.gamma(μ + p3.β_va + 1, λ*D_min) - SF.gamma(μ + p3.β_va + 1, λ*D_max))
+end
+# partially rimed ice or large unrimed ice
+function q_r(p3::PSP3, F_r::FT, N_0::FT, λ::FT, μ::FT, D_min::FT) where{FT} 
+    return (p3.α_va * N_0 /(1 - F_r)) * (λ)^(-1 * (μ + p3.β_va + 1)) * (SF.gamma(μ + p3.β_va + 1) + (SF.gamma(μ + p3.β_va + 1, λ*D_min) - (μ + p3.β_va)*SF.gamma(μ + p3.β_va)))
+end
+
+"""
+    q_gamma(p3, F_r, N_0, λ, μ, th)
+
+ - p3 - a struct with P3 scheme parameters 
+ - F_r - rime mass fraction [q_rim/q_i]
+ - N_0 - intercept parameter of N′ gamma distribution 
+ - λ - slope parameter of N′ gamma distribution 
+ - μ - shape parameter of N′ gamma distribution 
+ - th - thresholds() nonlinear solve output tuple (D_cr, D_gr, ρ_g, ρ_d)
+
+ Returns prognostic mass mixing ratio for all values of D 
+    Eq. 5 in Morrison and Milbrandt (2015).
+"""
+function q_gamma(p3::PSP3, F_r::FT, N_0::FT, λ::FT, μ::FT, th = (; D_cr = FT(0), D_gr = FT(0), ρ_g = FT(0), ρ_d = FT(0))) where{FT}
+    D_th = D_th_helper(p3) 
+
+    println("q_s = ", q_s(p3.ρ_i, N_0, λ, μ, FT(0), D_th))
+    println("q_n = ", q_n(p3, N_0, λ, μ, D_th, th.D_gr))
+    println("q_s 2 = ", q_s(th.ρ_g, N_0, λ, μ, th.D_gr, th.D_cr))
+    println("q_r = ", q_r(p3, F_r, N_0, λ, μ, th.D_cr))
+
+    if F_r == 0
+        return q_s(p3.ρ_i, N_0, λ, μ, FT(0), D_th) + q_r(p3, F_r, N_0, λ, μ, D_th)
+    else 
+        return q_s(p3.ρ_i, N_0, λ, μ, FT(0), D_th) + q_n(p3, N_0, λ, μ, D_th, th.D_gr) + q_s(th.ρ_g, N_0, λ, μ, th.D_gr, th.D_cr) + q_r(p3, F_r, N_0, λ, μ, th.D_cr)
+    end
+end
 
 """
     N′(D, p)
@@ -213,48 +286,9 @@ end
 N′(D, p) = p.N_0 * D ^ (p.μ) * exp(-p.λ * D)
 
 """
-    μ_calc(λ)
-
- - λ - slope parameter for gamma distribution of N′
-
- Returns the slope parameter (μ) corresponding to the given λ value
- Eq. 3 in Morrison and Milbrandt (2015).
 """
-μ_calc(λ) = 0.00191 * λ^(0.8) - 2
-
-"""
-    N_helper(N_0, λ)
-
- - N_0 - intercept parameter of N′ gamma distribution
- - λ - slope parameter of N′ gamma distribution 
-
-Returns the prognostic number mixing ratio 
-Eq. 4 in Morrison and Milbrandt (2015).
-"""
-function N_helper(N_0::FT, λ::FT) where {FT}
-    problem = IN.IntegralProblem(N′, 0, Inf, (N_0 = N_0, λ = λ, μ = μ_calc(λ)))
-    sol = IN.solve(problem, IN.HCubatureJL(), reltol = 1e-3, abstol = 1e-3)
-    return FT(sol.u)
-end
-
-"""
-    q_helper(N_0, λ)
-
- - p3 - a struct with P3 scheme parameters
- - N_0 - intercept parameter of N′ gamma distribution
- - λ - slope parameter of N′ gamma distribution 
- - F_r - rime mass fraction (q_rim/q_i)
- - ρ_r - rime density (q_rim/B_rim) [kg/m^3]
-
-Returns the prognostic mass mixing ratio
-Eq. 5 in Morrison and Milbrandt (2015).
-"""
-function q_helper(p3::PSP3{FT}, N_0::FT, λ::FT, F_r::FT, ρ_r::FT) where {FT}
-    th = thresholds(p3, ρ_r, F_r)
-    q′(D, p) = p3_mass(p3, D, F_r, th) * N′(D, p)
-    problem = IN.IntegralProblem(q′, 0, Inf, (N_0 = N_0, λ = λ, μ = μ_calc(λ)))
-    sol = IN.solve(problem, IN.HCubatureJL(), reltol = 1e-3, abstol = 1e-3)
-    return FT(sol.u)
+function N_0_helper(N::FT, λ::FT, μ::FT) where{FT}
+    return N/(λ^(-1 - μ) * SF.gamma(1 + μ))
 end
 
 """
@@ -274,14 +308,52 @@ for a given number mixing ratio (N) and mass mixing ratio (q).
 """
 function distribution_parameter_solver(p3::PSP3{FT}, q::FT, N::FT, ρ_r::FT, F_r::FT) where {FT}
 
-    shape_problem(λ) = q - N * q_helper(p3, FT(1), λ, F_r, ρ_r)/N_helper(FT(1), λ)
+    th = thresholds(p3, ρ_r, F_r)
+    
+    shape_problem(λ) = q - q_gamma(p3, F_r, N_0_helper(N, λ, μ_calc(λ)), λ, μ_calc(λ), th)  
 
-    λ = RS.find_zero(shape_problem, RS.SecantMethod(FT(0), FT(1000)), RS.CompactSolution(),).root 
+    λ = RS.find_zero(shape_problem, RS.SecantMethod(FT(1), FT(1e10)), RS.CompactSolution(), RS.RelativeSolutionTolerance(1e-3), 10,).root 
 
     return(;
         λ = λ,
-        N_0 = N/N_helper(FT(1), λ)
+        N_0 = N_0_helper(N, λ, μ_calc(λ))
     )
+end
+
+
+"""
+    N_helper(N_0, λ)
+
+ - N_0 - intercept parameter of N′ gamma distribution
+ - λ - slope parameter of N′ gamma distribution 
+
+Returns the prognostic number mixing ratio 
+Eq. 4 in Morrison and Milbrandt (2015).
+"""
+function N_helper(N_0::FT, λ::FT) where {FT}
+    problem = IN.IntegralProblem(N′, 0, Inf, (N_0 = N_0, λ = λ, μ = μ_calc(λ)))
+    sol = IN.solve(problem, IN.HCubatureJL(), reltol = 1e-10, abstol = 1e-10)
+    return FT(sol.u)
+end
+
+"""
+    q_helper(N_0, λ)
+
+ - p3 - a struct with P3 scheme parameters
+ - N_0 - intercept parameter of N′ gamma distribution
+ - λ - slope parameter of N′ gamma distribution 
+ - F_r - rime mass fraction (q_rim/q_i)
+ - ρ_r - rime density (q_rim/B_rim) [kg/m^3]
+
+Returns the prognostic mass mixing ratio
+Eq. 5 in Morrison and Milbrandt (2015).
+"""
+function q_helper(p3::PSP3{FT}, N_0::FT, λ::FT, F_r::FT, ρ_r::FT) where {FT}
+    th = thresholds(p3, ρ_r, F_r)
+    q′(D, p) = p3_mass(p3, D, F_r, th) * N′(D, p)
+    problem = IN.IntegralProblem(q′, 0, Inf, (N_0 = N_0, λ = λ, μ = μ_calc(λ)))
+    sol = IN.solve(problem, IN.HCubatureJL(), reltol = 1e-10, abstol = 1e-10)
+    return FT(sol.u)
 end
 
 end
--- test/P3SchemeSandbox.jl
+++ test/P3SchemeSandbox.jl
@@ -2,20 +2,108 @@ import CloudMicrophysics as CM
 import CloudMicrophysics.P3Scheme as P3
 import CloudMicrophysics.Parameters as CMP
 import CLIMAParameters as CP
+import Integrals as IN
+import SpecialFunctions as SF
+import RootSolvers as RS
 
 FT = Float64
 
 const PSP3 = CMP.ParametersP3
 p3 = CMP.ParametersP3(FT)
 
-N = FT(100.0)
-q = FT(1e-1)
-F_r = FT(0.5)
-ρ_r = FT(1e-4)
-D = FT(1e-5)
+"""
+    N′(D, p)
+    
+ - D - maximum particle dimension
+ - p - a tuple containing N_0, λ, μ (intrcept, slope, and shape parameters for N′ respectively)
+ 
+ Returns the value of N′ 
+ Eq. 2 in Morrison and Milbrandt (2015).   
+"""
+N′(D, p) = p.N_0 * exp(-p.λ * D)
 
-#th = P3.thresholds(p3, ρ_r, F_r)
-#mass = P3.p3_mass(p3, D, F_r, th)
-(λ, N_0) = P3.distribution_parameter_solver(p3, q, N, ρ_r, F_r)
-println("λ = ", λ)
-println("N_0 = ", N_0)
+
+"""
+    q_helper(N_0, λ)
+
+ - p3 - a struct with P3 scheme parameters
+ - N_0 - intercept parameter of N′ gamma distribution
+ - λ - slope parameter of N′ gamma distribution 
+ - F_r - rime mass fraction (q_rim/q_i)
+ - ρ_r - rime density (q_rim/B_rim) [kg/m^3]
+
+Returns the prognostic mass mixing ratio
+Eq. 5 in Morrison and Milbrandt (2015).
+"""
+function q_helper(p3::PSP3{FT}, N_0::FT, λ::FT, F_r::FT, ρ_r::FT) where {FT}
+    th = P3.thresholds(p3, ρ_r, F_r)
+    q′(D, p) = P3.p3_mass(p3, D, F_r, th) * N′(D, p)
+    problem = IN.IntegralProblem(q′, 0, Inf, (N_0 = N_0, λ = λ))
+    sol = IN.solve(problem, IN.HCubatureJL(), reltol = 1e-3, abstol = 1e-3)
+    return FT(sol.u)
+end
+
+"""
+    lambda_expected()
+
+Returns the expected lambda to be compared to the non linear solver
+"""
+function λ_expected(q_i::FT, n_0::FT) where{FT}
+    m_e = FT(3)
+    r_0 = FT(1e-5)
+    ρ = FT(1)       # density of air 
+    ρ_i = FT(917)   # kg/m^3
+    m_0 = FT(4/3 * π * ρ_i * r_0^3)
+    X_m = FT(1)
+    Δ_m = FT(0)
+
+    return ((SF.gamma(m_e + Δ_m + 1) * X_m * m_0 * n_0)/(q_i * ρ * r_0 ^ (m_e + Δ_m)))^(1/(m_e + Δ_m + 1))
+
+end
+
+function test_1M(p3, q_i, n_0, F_r, ρ_r)
+    shape_problem(λ) = q_i - q_helper(p3, n_0, λ, F_r, ρ_r)
+    λ = RS.find_zero(shape_problem, RS.SecantMethod(FT(0), FT(1e6)), RS.CompactSolution(),).root 
+
+    println("True λ = ", λ_expected(q_i))       # need to calculate real lambda and then plug in bounds that work for it
+    println("Modeled λ = ", λ)
+end
+
+function test_solver(p3, q, N, ρ_r, F_r) 
+    (λ, N_0) = P3.distribution_parameter_solver(p3, q, N, ρ_r, F_r)
+    println("λ solved = ", λ) 
+    println("N_0 solved = ", N_0)
+end
+
+println("test")
+
+N = FT(1e8)
+q = FT(1e-3)
+q_r = FT(0.5 * 1e-6) 
+q_i = FT(1e-6)
+B_r = FT(1/200 * 1e-4)
+n_0 = FT(16 * 1e6)
+
+F_r = q_r/q_i
+ρ_r = ifelse(B_r == 0, FT(0), q_r/B_r)
+
+th = P3.thresholds(p3, ρ_r, F_r)
+λ_ex = λ_expected(q_i, n_0)
+q_calculated1 = P3.q_gamma(p3, F_r, n_0, λ_ex, 0.00191*λ_ex^0.8 - 2, th)
+
+N = n_0/λ_ex
+println("N_expected = ", N)
+# N = n_0 * λ^(-0.00191*λ_ex^(0.8) + 2 - 1) * SF.gamma(1 + 0.00191*λ_ex^0.8 - 2)
+
+q_calculated2 = P3.q_helper(p3, n_0, λ_ex, F_r, ρ_r)
+N_calculated = P3.N_helper(n_0, λ_ex)
+
+println("with gamma functions, q = ", q_calculated1)
+println("with integral, q = ", q_calculated2)
+println()
+
+println("n_0 entered = ", n_0)
+println("λ expected = ", λ_ex)
+
+# test_1M(p3, q_i, N, F_r, ρ_r)
+test_solver(p3, q_calculated1, N, ρ_r, F_r)
