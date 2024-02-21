08fa7cf4dd54bd1dd275017259be97f3407e8451
Anastasia Popova <apopova@caltech.edu>
2024-02-21 15:02:57 -0800

    terminal velocity start to implement

--- src/P3Scheme.jl
+++ src/P3Scheme.jl
@@ -228,7 +228,7 @@ Returns the shape parameter μ for a given λ value
 Eq. 3 in Morrison and Milbrandt (2015).
 """
 function DSD_μ(p3::PSP3, λ::FT) where {FT}
-    @assert λ > FT(0)
+    #@assert λ > FT(0)
     return min(p3.μ_max, max(FT(0), p3.a * λ^p3.b - p3.c))
 end
 
@@ -269,7 +269,8 @@ end
 # q_rim = 0 and D_min = D_th, D_max = inf
 function q_rz(p3::PSP3, N_0::FT, λ::FT, D_min::FT) where {FT}
     x = DSD_μ(p3, λ) + p3.β_va + 1
-    return α_va_si(p3) * N_0 / λ^x * (Γ(x, λ * D_min))
+    return α_va_si(p3) * N_0 / λ^x *
+           (Γ(x) + Γ(x, λ * D_min) - (x - 1) * Γ(x - 1))
 end
 # q_rim > 0 and D_min = D_th and D_max = D_gr
 function q_n(p3::PSP3, N_0::FT, λ::FT, D_min::FT, D_max::FT) where {FT}
@@ -280,7 +281,8 @@ end
 # q_rim > 0 and D_min = D_cr, D_max = inf
 function q_r(p3::PSP3, F_r::FT, N_0::FT, λ::FT, D_min::FT) where {FT}
     x = DSD_μ(p3, λ) + p3.β_va + 1
-    return α_va_si(p3) * N_0 / (1 - F_r) / λ^x * (Γ(x, λ * D_min))
+    return α_va_si(p3) * N_0 / (1 - F_r) / λ^x *
+           (Γ(x) + Γ(x, λ * D_min) - (x - 1) * Γ(x - 1))
 end
 
 """
@@ -318,6 +320,49 @@ function q_gamma(
 end
 
 """
+    get_bounds(N, N̂, q, F_r, p3, th)
+
+ - N - ice number concentration [1/m3]
+ - N̂ - normalization as set in distribution_parameter_solver()
+ - q - mass mixing ratio
+ - F_r -rime mass fraction [q_rim/q_i]
+ - p3 - a struct with P3 scheme parameters
+ - th -  thresholds() nonlinear solve output tuple (D_cr, D_gr, ρ_g, ρ_d)
+
+ Returns estimated guess for λ from q to be used in distribution_parameter_solver()
+"""
+function get_bounds(
+    N::FT,
+    N̂::FT,
+    q::FT,
+    F_r::FT,
+    p3::PSP3,
+    th = (; D_cr = FT(0), D_gr = FT(0), ρ_g = FT(0), ρ_d = FT(0)),
+) where {FT}
+
+    left = FT(1e2)
+    right = FT(1e6)
+    radius = FT(0.8)
+
+    ql = q_gamma(p3, F_r, N / N̂, log(left), th)
+    qr = q_gamma(p3, F_r, N / N̂, log(right), th)
+
+    guess =
+        left * (q / (N̂ * ql))^((log(right) - log(left)) / (log(qr) - log(ql)))
+
+    max = log(guess * exp(radius))
+    min = log(guess)
+
+    # Use constant bounds for small λ
+    if guess < FT(2.5 * 1e4) || isequal(guess, NaN)
+        min = log(FT(20000))
+        max = log(FT(50000))
+    end
+
+    return (; min, max)
+end
+
+"""
     distrbution_parameter_solver()
 
  - p3 - a struct with P3 scheme parameters
@@ -344,20 +389,88 @@ function distribution_parameter_solver(
 
     # To ensure that λ is positive solve for x such that λ = exp(x)
     # We divide by N̂ to deal with large N₀ values for Float32
-    N̂ = FT(1e30)
+    N̂ = FT(1e20)
     shape_problem(x) = q / N̂ - q_gamma(p3, F_r, N / N̂, x, th)
+
+    # Get intial guess for solver 
+    (; min, max) = get_bounds(N, N̂, q, F_r, p3, th)
 
     # Find slope parameter
     x =
         RS.find_zero(
             shape_problem,
-            RS.SecantMethod(FT(log(20000)), FT(log(50000))),
+            RS.SecantMethod(min, max),
             RS.CompactSolution(),
             RS.RelativeSolutionTolerance(eps(FT)),
             10,
         ).root
 
     return (; λ = exp(x), N_0 = DSD_N₀(p3, N, exp(x)))
+end
+
+"""
+    integrate(a, b, c1, c2, c3)
+
+ - a - lower bound 
+ - b - upper bound 
+ - c1, c2, c3 - respective constants 
+
+ Integrates the function c1 * D ^ (c2) * exp(-c3 * D) dD from a to b 
+    Returns the result
+"""
+function integrate(a::FT, b::FT, c1::FT, c2::FT, c3::FT) where{FT}
+    if b == Inf
+        return c1 * c3 ^ (-c2 - 1) * (Γ(1 + c2, a * c3))
+    elseif a == 0 
+        return c1 * c3 ^ (-c2 - 1) * (Γ(1 + c2) - Γ(1 + c2, b * c3))
+    else 
+        return c1 * c3 ^ (-c2 - 1) * (Γ(1 + c2, a * c3) - Γ(1 + c2, b * c3))
+    end
+end
+
+"""
+"""
+function terminal_velocity_mass(p3::PSP3, q::FT, N::FT, ρ_r::FT, F_r::FT) where{FT}
+
+    # Get the thresholds for different particles regimes
+    th = thresholds(p3, ρ_r, F_r)
+    D_th = D_th_helper(p3)
+
+    # Get the shape parameters
+    (λ, N_0) = distribution_parameter_solver(p3, q, N, ρ_r, F_r)
+    μ = DSD_μ(p3, λ)
+
+    # Get the ai, bi, ci constants for velocity calculations
+    
+
+    # Redefine α_va to be in si units
+    α_va = α_va_si(p3)
+
+    # STILL NEED TO BE SUMMED OVER RESPECTIVE ai bi and ci 
+    if F_r == 0 
+        term1 = integrate(0, D_th, π / 6 * p3.ρ_i * ai * N_0, bi + μ + 3, ci + λ)
+        term2 = integrate(D_th, Inf, α_va * ai * N_0 * (16 * p3.ρ_i ^ 2 * p3.γ^3/(9 * π * α_va ^ 2)) ^ κ, bi + p3.β_va + μ + κ * (3 * p3.σ - 2 * p3.β_va), ci + λ)
+    else 
+        term1 = integrate(0, D_th, π / 6 * p3.ρ_i * ai * N_0, bi + μ + 3, ci + λ)
+        term2 = integrate(D_th, th.D_gr, α_va * ai * N_0 * (16 * p3.ρ_i ^ 2 * p3.γ^3/(9 * π * α_va ^ 2)) ^ κ, bi + p3.β_va + μ + κ * (3 * p3.σ - 2 * p3.β_va), ci + λ)
+        term3 = integrate(th.D_gr, th.D_cr, π / 6 * th.ρ_g * ai * N_0 * (p3.ρ_i / th.ρ_g)^(2 * κ), bi + 3 + μ, ci + λ)
+        term4 = (
+            integrate(th.D_cr, Inf, 1/(1 - F_r) * α_va * ai * N_0 * (p3.γ * (1-F_r)) ^ (3 * κ), bi + p3.β_va + μ + κ(3 * p3.σ), ci + λ) + 
+            integrate(th.D_cr, Inf, 1/(1 - F_r) * α_va * ai * N_0 * (p3.γ ^ 2 * π * F_r * 3/4 * (1-F_r)^2) ^ κ, bi + p3.β_va + μ + κ(2 + 2 * p3.σ), ci + λ) + 
+            integrate(th.D_cr, Inf, 1/(1 - F_r) * α_va * ai * N_0 * (p3.γ * π ^ 2 * F_r ^ 2 * 3/16 * (1-F_r)) ^ κ, bi + p3.β_va + μ + κ(4 + p3.σ), ci + λ) + 
+            integrate(th.D_cr, Inf, 1/(1 - F_r) * α_va * ai * N_0 * (F_r^3 * π^3 / 64) ^ κ, bi + p3.β_va + μ + κ(6), ci + λ)
+        )
+    end
+
+    # Get total sums (top) 
+    return top / q
+
+end
+
+"""
+"""
+function terminal_velocity_number(p3::PSP3, N::FT, F_r::FT, ρ_r::FT)
+
 end
 
 end
