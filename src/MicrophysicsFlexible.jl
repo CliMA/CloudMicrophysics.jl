742c7ebe6e7774942baab6f20530de3e5a562b2f
Emily de Jong <edejong@caltech.edu>
2024-03-01 15:04:07 -0800

    Make cloudy deps an extension instead

--- src/MicrophysicsFlexible.jl
+++ src/MicrophysicsFlexible.jl
@@ -8,118 +8,32 @@ Flexible N-moment microphysics representation, including:
 """
 module MicrophysicsFlexible
 
-import Thermodynamics as TD
-import Thermodynamics.Parameters as TDP
-
-import ..Common as CO
-import ..Parameters as CMP
-
-import Cloudy as CL
-import Cloudy.ParticleDistributions as CPD
-import Cloudy.KernelFunctions as CLK
-
-export coalescence
-
-"""
-A structure containing the subdistributions, their moments, and additional
-dynamical parameters corresponding to rates of collision, sedimentation, and
-condensation/evaporation
-"""
-Base.@kwdef mutable struct CLSetup{FT} 
+mutable struct CLSetup{FT}
     "Subdistributions of the hydrometeor size distribution; defaults to
     exponential cloud mode and gamma rain mode"
-    pdists::Vector{CPD.PrimitiveParticleDistribution{FT}} = Vector([
-        CPD.ExponentialPrimitiveParticleDistribution(FT(0), FT(1.0)),
-        CPD.GammaPrimitiveParticleDistribution(FT(0), FT(1.0), FT(1.0))
-    ])
+    pdists::Vector
     "Moments of the subdistributions; should correspond with pdists"
-    mom::Vector{FT} = [FT(0) for i in 1:5]
+    mom::Vector{FT}
     "Total number of prognostic moments for each subdistribution"
-    NProgMoms::Vector{Int} = [Integer(CPD.nparams(dist)) for dist in pdists]
+    NProgMoms::Vector{Int}
     "Kernel function for collisional coalescence"
-    KernelFunc::CLK.KernelFunction{FT} = CLK.LongKernelFunction(FT(0.5236), FT(9.44e-3), FT(5.78e-3))
+    KernelFunc::Any
     "Particle mass thresholds for analytical integration style"
-    mass_thresholds::Vector{FT} = [10.0, Inf]
+    mass_thresholds::Vector{FT}
     "Polynomial order of the kernel approximation for coalescence"
-    kernel_order::Int = 1
+    kernel_order::Int
     "Upper limit for evaluation of the polynomial kernel approximation"
-    kernel_limit::FT = FT(500)
+    kernel_limit::FT
     "Coalescence data"
-    coal_data = nothing
+    coal_data::Any
     "Sedimentation rate parameters"
-    vel::Vector{Tuple{FT,FT}} = [(FT(2.0), FT(1.0 / 6))]
+    vel::Vector{Tuple{FT, FT}}
 end
 
-"""
-    coalescence(clinfo)
+function coalescence end
 
- - `clinfo` - kwarg structure containing pdists, moments, and coalescence parameters
- TODO: currently implemented only for analytical coalescence style
+function condensation end
 
-Returns a vector of moment tendencies due to collisional coalescence
-"""
-function coalescence(clinfo::CLSetup{FT}) where {FT}
-    kernel_tensor = CL.KernelTensors.CoalescenceTensor(clinfo.KernelFunc, clinfo.kernel_order, clinfo.kernel_limit)
-    if isnothing(clinfo.coal_data)
-        clinfo.coal_data = CL.Coalescence.initialize_coalescence_data(
-            CL.EquationTypes.AnalyticalCoalStyle(),
-            kernel_tensor,
-            clinfo.NProgMoms,
-            dist_thresholds = clinfo.mass_thresholds
-        )
-    end
-    # first, update the particle distributions
-    for (i, dist) in enumerate(clinfo.pdists)
-        ind_rng = CL.get_dist_moments_ind_range(clinfo.NProgMoms, i)
-        CPD.update_dist_from_moments!(dist, clinfo.mom[ind_rng])
-    end
-    CL.Coalescence.update_coal_ints!(
-        CL.EquationTypes.AnalyticalCoalStyle(),
-        clinfo.pdists,
-        clinfo.coal_data
-    )
-    return clinfo.coal_data.coal_ints
+function sedimentation end
+
 end
-
-"""
-    condensation(clinfo, aps, tps, q, ρ, T)
-
- - `clinfo` - kwarg structure containing pdists, moments, and coalescence parameters
- - `aps` - air properties
- - `tps` - thermodynamics parameters
- - `q` - phase partition
- - `ρ` - air density
- - `T` - air temperature
-Returns a vector of moment tendencies due to condensation/evaporation
-"""
-function condensation(
-    clinfo::CLSetup{FT},
-    aps::CMP.AirProperties{FT},
-    tps::TDP.ThermodynamicsParameters{FT},
-    q::TD.PhasePartition{FT},
-    ρ::FT,
-    T::FT,
-) where {FT}
-    S = TD.supersaturation(tps, q, ρ, T, TD.Liquid())
-    ξ = CO.G_func(aps, tps, T, TD.Liquid())
-
-    # first, update the particle distributions
-    for (i, dist) in enumerate(clinfo.pdists)
-        ind_rng = CL.get_dist_moments_ind_range(clinfo.NProgMoms, i)
-        CPD.update_dist_from_moments!(dist, clinfo.mom[ind_rng])
-    end
-    return CL.Condensation.get_cond_evap(S, (; ξ=ξ, pdists=clinfo.pdists))
-end
-
-"""
-    sedimentation(clinfo)
-
- - `clinfo` - kwarg structure containing pdists, moments, and coalescence parameters
-Returns the integrated fall speeds corresponding to the rate of change of prognostic moments
-"""
-function sedimentation(clinfo::CLSetup{FT}) where {FT}
-    return CL.Sedimentation.get_sedimentation_flux((; pdists=clinfo.pdists, vel=clinfo.vel))
-end
-
-
-end #module
--- test/microphysicsflexible_tests.jl
+++ test/microphysicsflexible_tests.jl
@@ -1,7 +1,6 @@
 import Test as TT
 
 import Thermodynamics as TD
-
 import CloudMicrophysics.Parameters as CMP
 import CloudMicrophysics.MicrophysicsFlexible as CMF
 import Cloudy.ParticleDistributions as CPD
@@ -16,16 +15,28 @@ function test_microphysics_flexible(FT)
 
     TT.@testset "Flexible microphysics - unit tests" begin
         # Create Cloudy struct with different distributions 
-        clinfo = CMF.CLSetup{FT}(
-            pdists = [CPD.LognormalPrimitiveParticleDistribution(FT(10.0), FT(1.0), FT(1.0))],
-            mom = CPD.get_moments(CPD.LognormalPrimitiveParticleDistribution(FT(10.0), FT(1.0), FT(1.0))),
+        clinfo = CMF.CLSetup{FT}(;
+            pdists = [
+                CPD.LognormalPrimitiveParticleDistribution(
+                    FT(10.0),
+                    FT(1.0),
+                    FT(1.0),
+                ),
+            ],
+            mom = CPD.get_moments(
+                CPD.LognormalPrimitiveParticleDistribution(
+                    FT(10.0),
+                    FT(1.0),
+                    FT(1.0),
+                ),
+            ),
             NProgMoms = [3],
             KernelFunc = CPK.ConstantKernelFunction(FT(1)),
             mass_thresholds = [FT(100.0)],
             kernel_order = 4,
-            kernel_limit = FT(100)
+            kernel_limit = FT(100),
         )
-        
+
         # Create Cloudy struct with defaults
         clinfo = CMF.CLSetup{FT}()
         TT.@test length(clinfo.pdists) == 2
@@ -38,7 +49,7 @@ function test_microphysics_flexible(FT)
         dmom = CMF.coalescence(clinfo)
         TT.@test ~isnothing(clinfo.coal_data)
         TT.@test all(dmom[1:clinfo.NProgMoms[1]] .< FT(0))
-        TT.@test all(dmom[end - 1:end] .> FT(0))
+        TT.@test all(dmom[(end - 1):end] .> FT(0))
 
         # Test evaporation
         ρ = FT(1.1)
@@ -48,10 +59,10 @@ function test_microphysics_flexible(FT)
         cond_evap = CMF.condensation(clinfo, aps, tps, q, ρ, T)
         # conserve number density
         TT.@test cond_evap[1] == FT(0)
-        TT.@test cond_evap[clinfo.NProgMoms[1]+1] == FT(0)
+        TT.@test cond_evap[clinfo.NProgMoms[1] + 1] == FT(0)
         # decrease mass density
         TT.@test cond_evap[2] < FT(0)
-        TT.@test cond_evap[clinfo.NProgMoms[1]+2] < FT(0)
+        TT.@test cond_evap[clinfo.NProgMoms[1] + 2] < FT(0)
 
         # Test condensation 
         q_tot = FT(2e-2)
@@ -59,7 +70,7 @@ function test_microphysics_flexible(FT)
         cond_evap = CMF.condensation(clinfo, aps, tps, q, ρ, T)
         # conserve number density
         TT.@test cond_evap[1] == FT(0)
-        TT.@test cond_evap[clinfo.NProgMoms[1]+1] == FT(0)
+        TT.@test cond_evap[clinfo.NProgMoms[1] + 1] == FT(0)
         # increase cloud mass density
         TT.@test cond_evap[2] > FT(0)
 
