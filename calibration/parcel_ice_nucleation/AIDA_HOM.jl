1682fcf9dd184842f73988a10a3faa6957f76f70
amylu00 <alu3@caltech.edu>
2024-03-05 14:27:41 -0800

    data in dropbox

--- calibration/parcel_ice_nucleation/AIDA_HOM.jl
+++ calibration/parcel_ice_nucleation/AIDA_HOM.jl
@@ -1,4 +1,4 @@
-import EDF
+import DelimitedFiles
 
 import EnsembleKalmanProcesses as EKP
 import EnsembleKalmanProcesses.ParameterDistributions
@@ -13,32 +13,33 @@ import CloudMicrophysics as CM
 import CloudMicrophysics.Parameters as CMP
 import Thermodynamics as TD
 
-# include(joinpath(
-#     pkgdir(CM),
-#     "calibration",
-#     "parcel_ice_nucleation",
-#     "AIDA_Chamber_Data",
-#     "homogeneous_freezing",
-#     "IN05_17","in05_17_aida.edf",
-# ))
-
 include(joinpath(pkgdir(CM), "parcel", "Parcel.jl"))
-# data_path = joinpath(
-#     pkgdir(CM),
-#     "calibration",
-#     "parcel_ice_nucleation",
-#     "AIDA_Chamber_Data",
-#     "homogeneous_freezing",
-#     "IN05_17","in05_17_aida.edf",
-# )
+FT = Float64
 
-data_path = joinpath(
+# Check for data file in AIDA_data folder
+fpath = joinpath(
     pkgdir(CM),
     "calibration",
     "parcel_ice_nucleation",
-    "AIDA_Chamber_Data",
-    "heterogeneous_freezing",
-    "IN03_21_mineral_dust","in03_21_aida.edf",
+    "AIDA_data",
 )
+data_file = "in05_17_aida.edf"
 
-EDF.read(data_path)
+if isfile(joinpath(fpath, data_file))
+    data_path = joinpath(fpath, data_file)
+else
+    # if data does not exist, download from dropbox
+    !isfile(joinpath(fpath, data_file))
+    url = "https://www.dropbox.com/scl/fi/zm38lvhdu4sm8um2fvz9y/in05_17_aida.edf?rlkey=156zoi14y4xix0e569ja0ftzv&dl=0"
+    download(url, data_file)
+    mkdir(joinpath(fpath, data_file))
+    mv(
+        joinpath(pkgdir(CM), data_file),
+        joinpath(fpath, data_file),
+        force = true,
+    )
+    data_path = joinpath(fpath, data_file)
+end
+
+
+IN0517 = DelimitedFiles.readdlm(data_path, skipstart = 125)
