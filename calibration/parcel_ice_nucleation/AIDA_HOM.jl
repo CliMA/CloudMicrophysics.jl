adb44721e899e4d44868aa12ee23a07a14069763
amylu00 <alu3@caltech.edu>
2024-03-04 16:24:10 -0800

    ill deal with this later

--- calibration/parcel_ice_nucleation/AIDA_HOM.jl
+++ calibration/parcel_ice_nucleation/AIDA_HOM.jl
@@ -13,11 +13,32 @@ import CloudMicrophysics as CM
 import CloudMicrophysics.Parameters as CMP
 import Thermodynamics as TD
 
-include(joinpath(
+# include(joinpath(
+#     pkgdir(CM),
+#     "calibration",
+#     "parcel_ice_nucleation",
+#     "AIDA_Chamber_Data",
+#     "homogeneous_freezing",
+#     "IN05_17","in05_17_aida.edf",
+# ))
+
+include(joinpath(pkgdir(CM), "parcel", "Parcel.jl"))
+# data_path = joinpath(
+#     pkgdir(CM),
+#     "calibration",
+#     "parcel_ice_nucleation",
+#     "AIDA_Chamber_Data",
+#     "homogeneous_freezing",
+#     "IN05_17","in05_17_aida.edf",
+# )
+
+data_path = joinpath(
     pkgdir(CM),
     "calibration",
     "parcel_ice_nucleation",
-    "AIDA_Chamber_Data/homogeneous_freezing/IN05_17/in05_17_aida.edf"
-))
+    "AIDA_Chamber_Data",
+    "heterogeneous_freezing",
+    "IN03_21_mineral_dust","in03_21_aida.edf",
+)
 
-include(joinpath(pkgdir(CM), "parcel", "Parcel.jl"))
+EDF.read(data_path)
