dd3cbb64b90f92f8d970279e5272d6c9911155cd
rorlija1 <rorlija1@swarthmore.edu>
2024-08-24 16:36:53 -0700

    catching limits for KiD

--- src/P3_processes.jl
+++ src/P3_processes.jl
@@ -76,41 +76,52 @@ function ice_melt(
     ρ_rim::FT,
     dt::FT,
 ) where {FT}
-    # process not dependent on F_liq
-    # (we want ice core shape params)
-    F_liq_ = FT(0)
-    # Get constants
-    (; ν_air, D_vapor, K_therm) = aps
-    L_f = TD.latent_heat_fusion(tps, Tₐ)
-    N_sc = ν_air / D_vapor
+    dLdt = FT(0)
+    dNdt = FT(0)
+    if L_ice > eps(FT) && N_ice > eps(FT)
+        # process not dependent on F_liq
+        # (we want ice core shape params)
+        F_liq_ = FT(0)
+        # Get constants
+        (; ν_air, D_vapor, K_therm) = aps
+        L_f = TD.latent_heat_fusion(tps, Tₐ)
+        N_sc = ν_air / D_vapor
 
-    # Get the P3 diameter distribution...
-    th = thresholds(p3, ρ_rim, F_rim)
-    (λ, N_0) =
-        distribution_parameter_solver(p3, L_ice, N_ice, ρ_rim, F_rim, F_liq_)
-    N(D) = N′ice(p3, D, λ, N_0)
-    # ... and D_max for the integral
-    bound = get_ice_bound(p3, λ, FT(1e-6))
+        # Get the P3 diameter distribution...
+        th = thresholds(p3, ρ_rim, F_rim)
+        (λ, N_0) = distribution_parameter_solver(
+            p3,
+            L_ice,
+            N_ice,
+            ρ_rim,
+            F_rim,
+            F_liq_,
+        )
+        N(D) = N′ice(p3, D, λ, N_0)
+        # ... and D_max for the integral
+        bound = get_ice_bound(p3, λ, FT(1e-6))
 
-    # Ice particle terminal velocity
-    v(D) = ice_particle_terminal_velocity(p3, D, Chen2022, ρₐ, F_rim, th)
-    # Reynolds number
-    N_Re(D) = D * v(D) / ν_air
-    # Ventillation factor
-    F_v(D) = p3.vent_a + p3.vent_b * N_sc^(1 / 3) * N_Re(D)^(1 / 2)
-    dmdD(D) = p3_dmdD(p3, D, F_rim, th)
+        # Ice particle terminal velocity
+        v(D) = ice_particle_terminal_velocity(p3, D, Chen2022, ρₐ, F_rim, th)
+        # Reynolds number
+        N_Re(D) = D * v(D) / ν_air
+        # Ventillation factor
+        F_v(D) = p3.vent_a + p3.vent_b * N_sc^(1 / 3) * N_Re(D)^(1 / 2)
+        dmdD(D) = p3_dmdD(p3, D, F_rim, th)
 
-    f(D) = 4 * K_therm / L_f * (Tₐ - p3.T_freeze) * dmdD(D) / D * F_v(D) * N(D)
-    # Integrate
-    (dLdt, error) = QGK.quadgk(d -> f(d), 0, bound, rtol = FT(1e-6))
+        f(D) =
+            4 * K_therm / L_f * (Tₐ - p3.T_freeze) * dmdD(D) / D * F_v(D) * N(D)
+        # Integrate
+        (dLdt, error) = QGK.quadgk(d -> f(d), 0, bound, rtol = FT(1e-6))
 
-    # only consider melting (not fusion)
-    dLdt = max(0, dLdt)
-    # compute change of N_ice proportional to change in L
-    dNdt = N_ice / L_ice * dLdt
+        # only consider melting (not fusion)
+        dLdt = max(0, dLdt)
+        # compute change of N_ice proportional to change in L
+        dNdt = N_ice / L_ice * dLdt
 
-    # ... and dont exceed the available number and mass of water droplets
-    dNdt = min(dNdt, N_ice / dt)
-    dLdt = min(dLdt, L_ice / dt)
+        # ... and dont exceed the available number and mass of water droplets
+        dNdt = min(dNdt, N_ice / dt)
+        dLdt = min(dLdt, L_ice / dt)
+    end
     return (; dNdt, dLdt)
 end
