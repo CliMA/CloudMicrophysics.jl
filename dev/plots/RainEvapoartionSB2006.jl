c51a07c361b952a11d82e356970a8db56d19bcda
Documenter.jl <documenter@juliadocs.github.io>
2025-06-28 00:39:53 +0000

    build based on 944723f

---
+++ dev/plots/RainEvapoartionSB2006.jl
@@ -0,0 +1,120 @@
+import CairoMakie as MK
+import SpecialFunctions as SF
+
+import ClimaParams as CP
+import CloudMicrophysics as CM
+import CloudMicrophysics.ThermodynamicsInterface as TDI
+import CloudMicrophysics.Parameters as CMP
+import CloudMicrophysics.Common as CO
+import CloudMicrophysics.Microphysics2M as CM2
+
+FT = Float64
+
+const tps = TDI.TD.Parameters.ThermodynamicsParameters(FT)
+const aps = CMP.AirProperties(FT)
+const SB2006 = CMP.SB2006(FT)
+
+function rain_evaporation_CPU(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, qₛ, ρ, Nᵣ, T)
+
+    evap_rate_0 = FT(0)
+    evap_rate_1 = FT(0)
+    #TODO - update afer  wf change in 2M
+    S = TDI.supersaturation_over_liquid(tps, qₜ, qₗ, qᵢ, ρ, T)
+
+    if (qᵣ > FT(0) && S < FT(0))
+
+        (; ν_air, D_vapor) = aps
+        (; av, bv, α, β, ρ0) = SB2006.evap
+        ρw = SB2006.pdf_r.ρw
+        x_star = SB2006.pdf_r.xr_min
+        G = CO.G_func_liquid(aps, tps, T)
+
+        (; xr_mean) = CM2.pdf_rain_parameters(SB2006.pdf_r, qᵣ, ρ, Nᵣ)
+        Dr = (FT(6) / FT(π) / ρw)^FT(1 / 3) * xr_mean^FT(1 / 3)
+
+        t_star = (FT(6) * x_star / xr_mean)^FT(1 / 3)
+        a_vent_0 = av * FT(SF.gamma(-1, t_star)) / FT(6)^FT(-2 / 3)
+        b_vent_0 =
+            bv * FT(SF.gamma((-1 / 2) + (3 / 2) * β, t_star)) /
+            FT(6)^FT(β / 2 - 1 / 2)
+
+        a_vent_1 = av * SF.gamma(FT(2)) / FT(6)^FT(1 / 3)
+        b_vent_1 =
+            bv * SF.gamma(FT(5 / 2) + FT(3 / 2) * β) / FT(6)^FT(β / 2 + 1 / 2)
+
+        N_Re = α * xr_mean^β * sqrt(ρ0 / ρ) * Dr / ν_air
+        Fv0 = a_vent_0 + b_vent_0 * (ν_air / D_vapor)^FT(1 / 3) * sqrt(N_Re)
+        Fv1 = a_vent_1 + b_vent_1 * (ν_air / D_vapor)^FT(1 / 3) * sqrt(N_Re)
+
+        evap_rate_0 = min(FT(0), FT(2) * FT(π) * G * S * Nᵣ * Dr * Fv0 / xr_mean)
+        evap_rate_1 = min(FT(0), FT(2) * FT(π) * G * S * Nᵣ * Dr * Fv1 / ρ)
+    end
+
+    return (; evap_rate_0, evap_rate_1)
+end
+
+qᵥ = FT(1e-2)
+qₗ = FT(0)
+qᵢ = FT(0)
+qᵣ = FT(1e-4)
+qₛ = FT(0)
+qₜ = qᵥ + qₗ + qᵢ + qᵣ + qₛ
+ρ = FT(1.1)
+Nᵣ = FT(1e8)
+T = FT(300)
+
+qᵣ_range = range(1e-9, stop = 1e-4, length = 1000)
+Nᵣ_range = range(1e6, stop = 1e9, length = 1000)
+T_range = range(273.15, stop = 273.15 + 50, length = 1000)
+
+#! format: off
+evap_qᵣ_0 = [rain_evaporation_CPU(SB2006, aps, tps, qₜ, qₗ, qᵢ, _qᵣ, qₛ, ρ, Nᵣ, T).evap_rate_0 for _qᵣ in qᵣ_range]
+evap_Nᵣ_0 = [rain_evaporation_CPU(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, qₛ, ρ, _Nᵣ, T).evap_rate_0 for _Nᵣ in Nᵣ_range]
+evap_T_0 = [rain_evaporation_CPU(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, qₛ, ρ, Nᵣ, _T).evap_rate_0 for _T in T_range]
+
+evap_qᵣ_0n = [CM2.rain_evaporation(SB2006, aps, tps, qₜ, qₗ, qᵢ, _qᵣ, ρ, Nᵣ, T).evap_rate_0 for _qᵣ in qᵣ_range]
+evap_Nᵣ_0n = [CM2.rain_evaporation(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, ρ, _Nᵣ, T).evap_rate_0 for _Nᵣ in Nᵣ_range]
+evap_T_0n = [CM2.rain_evaporation(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, ρ, Nᵣ, _T).evap_rate_0 for _T in T_range]
+
+evap_qᵣ_3 = [rain_evaporation_CPU(SB2006, aps, tps, qₜ, qₗ, qᵢ, _qᵣ, qₛ, ρ, Nᵣ, T).evap_rate_1 for _qᵣ in qᵣ_range]
+evap_Nᵣ_3 = [rain_evaporation_CPU(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, qₛ, ρ, _Nᵣ, T).evap_rate_1 for _Nᵣ in Nᵣ_range]
+evap_T_3 = [rain_evaporation_CPU(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, qₛ, ρ, Nᵣ, _T).evap_rate_1 for _T in T_range]
+
+evap_qᵣ_3n = [CM2.rain_evaporation(SB2006, aps, tps, qₜ, qₗ, qᵢ, _qᵣ, ρ, Nᵣ, T).evap_rate_1 for _qᵣ in qᵣ_range]
+evap_Nᵣ_3n = [CM2.rain_evaporation(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, ρ, _Nᵣ, T).evap_rate_1 for _Nᵣ in Nᵣ_range]
+evap_T_3n = [CM2.rain_evaporation(SB2006, aps, tps, qₜ, qₗ, qᵢ, qᵣ, ρ, Nᵣ, _T).evap_rate_1 for _T in T_range]
+
+fig = MK.Figure(resolution = (800, 600))
+
+ax1 = MK.Axis(fig[1, 1], xlabel = "q_rain [g/kg]", ylabel = "evap rate [1/cm3/s]")
+ax2 = MK.Axis(fig[2, 1], xlabel = "N_rain [1/cm3]", ylabel = "evap rate [1/cm3/s]")
+ax3 = MK.Axis(fig[3, 1], xlabel = "T [K]", ylabel = "evap rate [1/cm3/s]")
+
+ax4 = MK.Axis(fig[1, 2], xlabel = "q_rain [g/kg]", ylabel = "evap rate [g/kg/s]")
+ax5 = MK.Axis(fig[2, 2], xlabel = "N_rain [1/cm3]", ylabel = "evap rate [g/kg/s]")
+ax6 = MK.Axis(fig[3, 2], xlabel = "T [K]", ylabel = "evap rate [g/kg/s]")
+
+MK.lines!(ax1, qᵣ_range .* 1e3, evap_qᵣ_0 .* 1e-6, color = :blue, label="exact")
+MK.lines!(ax1, qᵣ_range .* 1e3, evap_qᵣ_0n .* 1e-6, color = :orange, label="approx")
+MK.lines!(ax2, Nᵣ_range .* 1e-6, evap_Nᵣ_0 .* 1e-6, color =:blue)
+MK.lines!(ax2, Nᵣ_range .* 1e-6, evap_Nᵣ_0n .* 1e-6, color =:orange)
+MK.lines!(ax3, T_range, evap_T_0 .* 1e-6, color =:blue)
+MK.lines!(ax3, T_range, evap_T_0n .* 1e-6, color =:orange)
+MK.lines!(ax4, qᵣ_range .* 1e3, evap_qᵣ_3 .* 1e3, color =:blue)
+MK.lines!(ax4, qᵣ_range .* 1e3, evap_qᵣ_3n .* 1e3, color =:orange)
+MK.lines!(ax5, Nᵣ_range .* 1e-6, evap_Nᵣ_3 .* 1e3, color =:blue)
+MK.lines!(ax5, Nᵣ_range .* 1e-6, evap_Nᵣ_3n .* 1e3, color =:orange)
+MK.lines!(ax6, T_range, evap_T_3 .* 1e3, color =:blue)
+MK.lines!(ax6, T_range, evap_T_3n .* 1e3, color =:orange)
+#! format: on
+
+MK.axislegend(
+    ax1,
+    framevisible = false,
+    labelsize = 12,
+    orientation = :horizontal,
+    nbanks = 2,
+    position = :rt,
+)
+
+MK.save("SB2006_rain_evaporation.svg", fig)
---
+++ v0.18.0/plots/ARGplots.jl
@@ -0,0 +1,462 @@
+import Plots
+
+import CloudMicrophysics
+import ClimaParams
+import Thermodynamics
+
+const PL = Plots
+const AM = CloudMicrophysics.AerosolModel
+const AA = CloudMicrophysics.AerosolActivation
+const CP = ClimaParams
+const CMP = CloudMicrophysics.Parameters
+const TD = Thermodynamics
+
+FT = Float64
+
+tps = Thermodynamics.Parameters.ThermodynamicsParameters(FT)
+aip = CMP.AirProperties(FT)
+ap = CMP.AerosolActivationParameters(FT)
+
+include("plots/ARGdata.jl")
+
+# Atmospheric conditions
+T = 294.0         # air temperature
+p = 1000.0 * 1e2   # air pressure
+
+# We need the phase partition here only so that we can compute the
+# moist R_m and cp_m in aerosol activation module.
+# We are assuming here saturated conditions and no liquid water or ice.
+# This is consistent with the assumptions of the aerosol activation scheme.
+p_vs = TD.saturation_vapor_pressure(tps, T, TD.Liquid())
+q_vs = 1 / (1 - TD.Parameters.molmass_ratio(tps) * (p_vs - p) / p_vs)
+q = TD.PhasePartition(q_vs, 0.0, 0.0)
+
+# Sulfate
+sulfate = CMP.Sulfate(FT)
+
+# Insoluble material from ARG2000
+ϵ_insol = 0.0         # solubility of insol
+ϕ_insol = 0.0         # osmotic coeff of insol
+M_insol = 0.044       # molar mass of insol
+ν_insol = 0.0         # ions of salt in insol
+ρ_insol = 1770.0      # density of insol
+κ_insol = 0.0         # hygroscopicity of insol
+
+function mass2vol(mass_mixing_ratios)
+    if length(mass_mixing_ratios) == 2
+        densities = (sulfate.ρ, ρ_insol)
+    else
+        densities = (sulfate.ρ,)
+    end
+    volfractions =
+        (mass_mixing_ratios ./ densities) ./
+        sum(mass_mixing_ratios ./ densities)
+    return volfractions
+end
+
+# Abdul-Razzak and Ghan 2000
+# https://doi.org/10.1029/1999JD901161
+function make_ARG_figX(X)
+    p1 = PL.plot()
+    p2 = PL.plot()
+
+    for v_B in (true, false)
+        # mode 1 definitions
+        r_dry_1 = 0.05 * 1e-6 # um
+        stdev_1 = 2.0         # -
+        N_1 = 100.0 * 1e6   # 1/m3
+
+        if X in (1, 4)
+            vol_mixing_ratios_1 = (1.0,)
+            mass_mixing_ratios_1 = (1.0,)
+            n_components_1 = 1
+            if v_B
+                paper_mode_1 = AM.Mode_B(
+                    r_dry_1,
+                    stdev_1,
+                    N_1,
+                    mass_mixing_ratios_1,
+                    (sulfate.ϵ,),
+                    (sulfate.ϕ,),
+                    (sulfate.M,),
+                    (sulfate.ν,),
+                    (sulfate.ρ,),
+                    n_components_1,
+                )
+            else
+                paper_mode_1 = AM.Mode_κ(
+                    r_dry_1,
+                    stdev_1,
+                    N_1,
+                    vol_mixing_ratios_1,
+                    mass_mixing_ratios_1,
+                    (sulfate.M,),
+                    (sulfate.κ,),
+                    n_components_1,
+                )
+            end
+        end
+
+        if X in (2, 3, 5)
+            vol_mixing_ratios_1 = (1.0, 0.0)
+            mass_mixing_ratios_1 = (1.0, 0.0)
+            n_components_1 = 2
+            if v_B
+                paper_mode_1 = AM.Mode_B(
+                    r_dry_1,
+                    stdev_1,
+                    N_1,
+                    mass_mixing_ratios_1,
+                    (sulfate.ϵ, ϵ_insol),
+                    (sulfate.ϕ, ϕ_insol),
+                    (sulfate.M, M_insol),
+                    (sulfate.ν, ν_insol),
+                    (sulfate.ρ, ρ_insol),
+                    n_components_1,
+                )
+            else
+                paper_mode_1 = AM.Mode_κ(
+                    r_dry_1,
+                    stdev_1,
+                    N_1,
+                    vol_mixing_ratios_1,
+                    mass_mixing_ratios_1,
+                    (sulfate.M, M_insol),
+                    (sulfate.κ, κ_insol),
+                    n_components_1,
+                )
+            end
+        end
+
+        # parcel and mode 2 definitions
+        len = 100
+        global it = 1
+        act_frac1 = Vector{Float64}(undef, len)
+        act_frac2 = Vector{Float64}(undef, len)
+
+        stdev_2 = 2.0   # -
+        if X == 1
+            w = 0.5                                         # vertical velocity, m/s
+            r_dry_2 = 0.05 * 1e-6                           # um
+            N_2 = range(100, stop = 5000, length = len) * 1e6   # 1/m3
+            n_components_2 = 1                              # 1 mode
+            mass_mixing_ratios_2 = (1.0,)                   # all sulfate
+            vol_mixing_ratios_2 = (1.0,)                    # all sulfate
+
+            for N2i in N_2
+                if v_B
+                    paper_mode_2 = AM.Mode_B(
+                        r_dry_2,
+                        stdev_2,
+                        N2i,
+                        mass_mixing_ratios_2,
+                        (sulfate.ϵ,),
+                        (sulfate.ϕ,),
+                        (sulfate.M,),
+                        (sulfate.ν,),
+                        (sulfate.ρ,),
+                        n_components_2,
+                    )
+                else
+                    paper_mode_2 = AM.Mode_κ(
+                        r_dry_2,
+                        stdev_2,
+                        N2i,
+                        vol_mixing_ratios_2,
+                        mass_mixing_ratios_2,
+                        (sulfate.M,),
+                        (sulfate.κ,),
+                        n_components_2,
+                    )
+                end
+                AD = AM.AerosolDistribution((paper_mode_1, paper_mode_2))
+                act_frac1[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[1] /
+                    N_1
+                act_frac2[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[2] /
+                    N2i
+                global it += 1
+            end
+
+            x1_obs = Fig1_x_obs
+            y1_obs = Fig1_y_obs
+            x1_param = Fig1_x_param
+            y1_param = Fig1_y_param
+
+            x2_obs = Fig1_x_obs
+            y2_obs = Fig1_y_obs
+            x2_param = Fig1_x_param
+            y2_param = Fig1_y_param
+
+            xvar = N_2 * 1e-6
+            xlabel = "Mode 2 aerosol number concentration [1/cm3]"
+        elseif X == 2
+            w = 0.5                                         # vertical velocity, m/s
+            r_dry_2 = 0.05 * 1e-6                           # um
+            N_2 = range(100, stop = 5000, length = len) * 1e6   # 1/m3
+            n_components_2 = 2                              # 2 modes
+            mass_mixing_ratios_2 = (0.1, 0.9)                # 10% sulfate, 90% insoluble
+            vol_mixing_ratios_2 = mass2vol(mass_mixing_ratios_2)
+
+            for N2i in N_2
+                if v_B
+                    paper_mode_2 = AM.Mode_B(
+                        r_dry_2,
+                        stdev_2,
+                        N2i,
+                        mass_mixing_ratios_2,
+                        (sulfate.ϵ, ϵ_insol),
+                        (sulfate.ϕ, ϕ_insol),
+                        (sulfate.M, M_insol),
+                        (sulfate.ν, ν_insol),
+                        (sulfate.ρ, ρ_insol),
+                        n_components_2,
+                    )
+                else
+                    paper_mode_2 = AM.Mode_κ(
+                        r_dry_2,
+                        stdev_2,
+                        N2i,
+                        vol_mixing_ratios_2,
+                        mass_mixing_ratios_2,
+                        (sulfate.M, M_insol),
+                        (sulfate.κ, κ_insol),
+                        n_components_2,
+                    )
+                end
+                AD = AM.AerosolDistribution((paper_mode_1, paper_mode_2))
+                act_frac1[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[1] /
+                    N_1
+                act_frac2[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[2] /
+                    N2i
+                global it += 1
+            end
+
+            x1_obs = Fig2a_x_obs
+            y1_obs = Fig2a_y_obs
+            x1_param = Fig2a_x_param
+            y1_param = Fig2a_y_param
+
+            x2_obs = Fig2b_x_obs
+            y2_obs = Fig2b_y_obs
+            x2_param = Fig2b_x_param
+            y2_param = Fig2b_y_param
+
+            xvar = N_2 * 1e-6
+            xlabel = "Mode 2 aerosol number concentration [1/cm3]"
+        elseif X == 3
+            w = 0.5                                     # vertical velocity, m/s
+            r_dry_2 = 0.05 * 1e-6                       # um
+            N_2 = 100 * 1e6                             # 1/m3
+            n_components_2 = 2                          # 2 modes
+            # ranging from 10% to 100% sulfate, 90% to 0% insoluble
+            xvar = range(0.1, stop = 1, length = len)
+            mass_mixing_ratios_2 = [(i, 1 - i) for i in xvar]
+
+            for mmr2i in mass_mixing_ratios_2
+                vmr2i = mass2vol(mmr2i)
+                if v_B
+                    paper_mode_2 = AM.Mode_B(
+                        r_dry_2,
+                        stdev_2,
+                        N_2,
+                        mmr2i,
+                        (sulfate.ϵ, ϵ_insol),
+                        (sulfate.ϕ, ϕ_insol),
+                        (sulfate.M, M_insol),
+                        (sulfate.ν, ν_insol),
+                        (sulfate.ρ, ρ_insol),
+                        n_components_2,
+                    )
+                else
+                    paper_mode_2 = AM.Mode_κ(
+                        r_dry_2,
+                        stdev_2,
+                        N_2,
+                        vmr2i,
+                        mmr2i,
+                        (sulfate.M, M_insol),
+                        (sulfate.κ, κ_insol),
+                        n_components_2,
+                    )
+                end
+                AD = AM.AerosolDistribution((paper_mode_1, paper_mode_2))
+                act_frac1[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[1] /
+                    N_1
+                act_frac2[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[2] /
+                    N_2
+                global it += 1
+            end
+
+            x1_obs = Fig3a_x_obs
+            y1_obs = Fig3a_y_obs
+            x1_param = Fig3a_x_param
+            y1_param = Fig3a_y_param
+
+            x2_obs = Fig3b_x_obs
+            y2_obs = Fig3b_y_obs
+            x2_param = Fig3b_x_param
+            y2_param = Fig3b_y_param
+
+            xlabel = "Mode 2 soluble fraction (fraction of sulfate)"
+        elseif X == 4
+            w = 0.5                                     # vertical velocity, m/s
+            r_dry_2 = range(0.01, stop = 0.5, length = len) * 1e-6 # um
+            N_2 = 100 * 1e6                             # 1/m3
+            n_components_2 = 1                          # 1 mode
+            mass_mixing_ratios_2 = (1.0,)               # all sulfate
+            vol_mixing_ratios_2 = mass2vol(mass_mixing_ratios_2)
+
+            for rd2i in r_dry_2
+                if v_B
+                    paper_mode_2 = AM.Mode_B(
+                        rd2i,
+                        stdev_2,
+                        N_2,
+                        mass_mixing_ratios_2,
+                        (sulfate.ϵ,),
+                        (sulfate.ϕ,),
+                        (sulfate.M,),
+                        (sulfate.ν,),
+                        (sulfate.ρ,),
+                        n_components_2,
+                    )
+                else
+                    paper_mode_2 = AM.Mode_κ(
+                        rd2i,
+                        stdev_2,
+                        N_2,
+                        vol_mixing_ratios_2,
+                        mass_mixing_ratios_2,
+                        (sulfate.M,),
+                        (sulfate.κ,),
+                        n_components_2,
+                    )
+                end
+                AD = AM.AerosolDistribution((paper_mode_1, paper_mode_2))
+                act_frac1[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[1] /
+                    N_1
+                act_frac2[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, w, q)[2] /
+                    N_2
+                global it += 1
+            end
+
+            x1_obs = Fig4a_x_obs
+            y1_obs = Fig4a_y_obs
+            x1_param = Fig4a_x_param
+            y1_param = Fig4a_y_param
+
+            x2_obs = Fig4b_x_obs
+            y2_obs = Fig4b_y_obs
+            x2_param = Fig4b_x_param
+            y2_param = Fig4b_y_param
+
+            xvar = r_dry_2 * 1e6
+            xlabel = "Mode 2 mean radius [um]"
+        elseif X == 5
+            w = range(0.01, stop = 5, length = len)     # vertical velocity, m/s
+            r_dry_2 = 0.05 * 1e-6                       # um
+            N_2 = 100 * 1e6                             # 1/m3
+            n_components_2 = 2                          # 2 modes
+            mass_mixing_ratios_2 = (0.1, 0.9)           # 10% sulfate, 90% insoluble
+            vol_mixing_ratios_2 = mass2vol(mass_mixing_ratios_2)
+
+            if v_B
+                paper_mode_2 = AM.Mode_B(
+                    r_dry_2,
+                    stdev_2,
+                    N_2,
+                    mass_mixing_ratios_2,
+                    (sulfate.ϵ, ϵ_insol),
+                    (sulfate.ϕ, ϕ_insol),
+                    (sulfate.M, M_insol),
+                    (sulfate.ν, ν_insol),
+                    (sulfate.ρ, ρ_insol),
+                    n_components_2,
+                )
+            else
+                paper_mode_2 = AM.Mode_κ(
+                    r_dry_2,
+                    stdev_2,
+                    N_2,
+                    vol_mixing_ratios_2,
+                    mass_mixing_ratios_2,
+                    (sulfate.M, M_insol),
+                    (sulfate.κ, κ_insol),
+                    n_components_2,
+                )
+            end
+            AD = AM.AerosolDistribution((paper_mode_1, paper_mode_2))
+
+            for wi in w
+                act_frac1[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, wi, q)[1] /
+                    N_1
+                act_frac2[it] =
+                    AA.N_activated_per_mode(ap, AD, aip, tps, T, p, wi, q)[2] /
+                    N_2
+                global it += 1
+            end
+
+            x1_obs = Fig5a_x_obs
+            y1_obs = Fig5a_y_obs
+            x1_param = Fig5a_x_param
+            y1_param = Fig5a_y_param
+
+            x2_obs = Fig5b_x_obs
+            y2_obs = Fig5b_y_obs
+            x2_param = Fig5b_x_param
+            y2_param = Fig5b_y_param
+
+            xvar = w
+            xlabel = "Vertical velocity, w [m/s]"
+        end
+        v_B ? label = "CliMA-B" : label = "CliMA-κ"
+        PL.plot!(
+            p1,
+            xvar,
+            act_frac1,
+            label = label,
+            ylim = [0, 1],
+            ylabel = "Mode 1 act frac",
+            title = "ARG2000 Fig " * string(X),
+        )
+        PL.plot!(
+            p2,
+            xvar,
+            act_frac2,
+            legend = false,
+            ylim = [0, 1],
+            xlabel = xlabel,
+            ylabel = "Mode 2 act frac",
+        )
+        if v_B == false
+            PL.scatter!(
+                p1,
+                x1_obs,
+                y1_obs,
+                markercolor = :black,
+                label = "ARG2000 observations",
+            )
+            PL.plot!(
+                p1,
+                x1_param,
+                y1_param,
+                linecolor = :black,
+                label = "ARG2000 parameterization",
+            )
+            PL.scatter!(p2, x2_obs, y2_obs, markercolor = :black)
+            PL.plot!(p2, x2_param, y2_param, linecolor = :black)
+        end
+    end
+
+    PL.plot(p1, p2, layout = (2, 1))
+    PL.savefig("Abdul-Razzak_and_Ghan_fig_" * string(X) * ".svg")
+end
