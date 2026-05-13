6cae8f00b975e70e375449aef3a43aa05bd12d61
Anna Jaruga <ajaruga@caltech.edu>
2026-05-12 17:31:15 -0700

    ...

--- src/BulkMicrophysicsTendencies.jl
+++ src/BulkMicrophysicsTendencies.jl
@@ -368,7 +368,7 @@ Returns a `NamedTuple` containing the nonzero entries of `M` and `e`.
 
     #S_acnv_icl = CM1.conv_q_icl_to_q_sno_no_supersat(sno.acnv1M, q_icl, true)
     S_acnv_icl = CM1.conv_q_icl_to_q_sno(
-        icl, aps, tps, q_tot, q_lcl, q_icl, q_rai, q_sno, ρ, T,
+        icl, aps, tps, q_tot, q_lcl, q_icl, q_rai, q_sno, ρ, T, 1,
     )
     D = S_acnv_icl / max(q_min, q_icl)
     M22 -= D
